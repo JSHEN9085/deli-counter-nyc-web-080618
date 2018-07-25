6b9ff8315eb5588a5408908f526d385e4c39e563
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:17:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,14 @@
 # Write your code here.
-def line(array) 
-  if array.count == 0 
-    return "The line is currently empty."
-end 
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
