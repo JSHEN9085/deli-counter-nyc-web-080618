9dd9129588faa937c84b07bf2e4b452a32ddb00a
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:22:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(line, new_person)
 end
 
 def now_serving(line)
-  if line.count == 0 
+  if line.length == 0 
     puts"There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}." 
